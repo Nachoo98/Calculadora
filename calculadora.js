6e2b7f491f210a4803bdf478352b56e3ad904d82
Nachoo98 <gonzalezignacio19@gmail.com>
2021-12-22 19:29:41 -0300

    Creando la calculadora

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+let sumar=require('./sumar/sumar');
+let restar=require('./restar/restar');
+let multiplicar=require('./multiplicar/multiplicar');
+let dividir=require('./dividir/dividir');
+
+
+console.log(sumar(3,5));
+console.log(restar(3,5));
+console.log(multiplicar(3,5));
+console.log(dividir(3,5));
